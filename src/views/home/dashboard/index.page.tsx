a344af72d8884bae6bc0988f4ae059cc5e38f2cc
kangod <kangod.yan@gmail.com>
2023-09-27 16:35:04 +0800

    Style: 去掉tailwind的命名前缀，以适配tremor，并解决一个.container.tw-container去掉tw-之后出现的循环依赖问题（删除了后者container）

--- src/views/home/dashboard/index.page.tsx
+++ src/views/home/dashboard/index.page.tsx
@@ -1,13 +1,3 @@
-import { Card, Text, Metric, Flex, ProgressBar } from '@tremor/react';
-
-export default () => (
-    <Card className="max-w-sm">
-        <Text>Sales</Text>
-        <Metric>$ 71,465</Metric>
-        <Flex className="mt-4">
-            <Text>32% of annual target</Text>
-            <Text>$ 225,000</Text>
-        </Flex>
-        <ProgressBar value={32} className="mt-2" />
-    </Card>
-);
+export default () => {
+    return <div>home dashboard</div>;
+};
