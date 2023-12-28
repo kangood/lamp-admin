55e237e1f66f8966dcbb74a0a4649ca4ed7c9ecb
kangod <kangod.yan@gmail.com>
2023-12-28 14:55:05 +0800

    feat: 首页布局提交，使用 antv 统计图和 pv 显示

--- src/views/home/list.page.tsx
+++ src/views/home/list.page.tsx
@@ -1,6 +1,199 @@
-import { Avatar } from 'antd';
+import { Avatar, Button, Divider, Tabs, TabsProps } from 'antd';
+import { Column } from '@antv/g2plot';
+import { useEffect, useRef } from 'react';
+
+const data = [
+    {
+        name: '您',
+        date: '2023-12-20',
+        pv: 34,
+    },
+    {
+        name: '总数',
+        date: '2023-12-20',
+        pv: 38,
+    },
+    {
+        name: '您',
+        date: '2023-12-21',
+        pv: 27,
+    },
+    {
+        name: '总数',
+        date: '2023-12-21',
+        pv: 29,
+    },
+    {
+        name: '您',
+        date: '2023-12-22',
+        pv: 23,
+    },
+    {
+        name: '总数',
+        date: '2023-12-22',
+        pv: 24,
+    },
+    {
+        name: '您',
+        date: '2023-12-23',
+        pv: 19,
+    },
+    {
+        name: '总数',
+        date: '2023-12-23',
+        pv: 35,
+    },
+    {
+        name: '您',
+        date: '2023-12-24',
+        pv: 10,
+    },
+    {
+        name: '总数',
+        date: '2023-12-24',
+        pv: 10,
+    },
+    {
+        name: '您',
+        date: '2023-12-25',
+        pv: 4,
+    },
+    {
+        name: '总数',
+        date: '2023-12-25',
+        pv: 5,
+    },
+    {
+        name: '您',
+        date: '2023-12-26',
+        pv: 30,
+    },
+    {
+        name: '总数',
+        date: '2023-12-26',
+        pv: 33,
+    },
+    {
+        name: '您',
+        date: '2023-12-27',
+        pv: 29,
+    },
+    {
+        name: '总数',
+        date: '2023-12-27',
+        pv: 31,
+    },
+    {
+        name: '您',
+        date: '2023-12-28',
+        pv: 18,
+    },
+    {
+        name: '总数',
+        date: '2023-12-28',
+        pv: 23,
+    },
+];
 
 export default () => {
+    const ref = useRef(null);
+    // 图表。保证首次加载，且只加载一次
+    useEffect(() => {
+        if (!ref.current) {
+            return;
+        }
+        const stackedColumnPlot = new Column(ref.current, {
+            data,
+            isGroup: true,
+            xField: 'date',
+            yField: 'pv',
+            seriesField: 'name',
+            /** 设置颜色 */
+            // color: ['#1ca9e6', '#f88c24'],
+            /** 设置间距 */
+            // marginRatio: 0.1,
+            label: {
+                // 可手动配置 label 数据标签位置
+                position: 'middle', // 'top', 'middle', 'bottom'
+                // 可配置附加的布局方法
+                layout: [
+                    // 柱形图数据标签位置自动调整
+                    { type: 'interval-adjust-position' },
+                    // 数据标签防遮挡
+                    { type: 'interval-hide-overlap' },
+                    // 数据标签文颜色自动调整
+                    { type: 'adjust-color' },
+                ],
+            },
+            // 柱子样式
+            // columnStyle: {
+            //     radius: [20, 20, 0, 0],
+            // },
+        });
+        stackedColumnPlot.render();
+        // 返回清理函数，以确保在下一次运行之前清理上一次的实例
+        // eslint-disable-next-line consistent-return
+        return () => {
+            stackedColumnPlot.destroy();
+        };
+    }, [ref.current]);
+    // 标签切换 items
+    const items: TabsProps['items'] = [
+        {
+            key: '1',
+            label: '项目介绍',
+            children: (
+                <div>
+                    <div className="m-4">
+                        <span>产品名称</span>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <Button className="bg-[#e8f4ff] text-blue-500">Tealamp 快速开发平台</Button>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <span>立即去点个star吧~</span>
+                    </div>
+                    <Divider orientation="right">⭐ </Divider>
+                    <div className="m-4">
+                        <span>账号密码</span>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <Button className="text-blue-500 border-blue-500">
+                            平台管理员 (lamp_pt)
+                        </Button>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <Button className="text-green-500 border-green-500">
+                            超级管理员 (lamp)
+                        </Button>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <Button> 普通用户 (normal)</Button>
+                    </div>
+                    <Divider orientation="right">密码密码密码密码密码都是：123456</Divider>
+                    <div className="m-4">
+                        <span>获取源码</span>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <Button
+                            className="bg-red-400 text-white"
+                            href="https://github.com/KangodYan/tealamp-api-nestjs"
+                            target="_blank"
+                        >
+                            Tealamp 后端服务
+                        </Button>
+                        <div className="inline-block w-[1px] h-4 mx-3 bg-[#dcdfe6]" />
+                        <Button
+                            className="bg-green-400 text-white"
+                            href="https://github.com/KangodYan/tealamp-admin-react"
+                            target="_blank"
+                        >
+                            Tealamp 后台管理 (本项目)
+                        </Button>
+                    </div>
+                </div>
+            ),
+        },
+        {
+            key: '2',
+            label: '技术栈',
+            children: 'Content of Tab Pane 2',
+        },
+    ];
     return (
         <div>
             <section className="bg-white dark:bg-slate-900">
@@ -39,6 +232,15 @@ export default () => {
                             <p className="leading-relaxed">总访问量</p>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section className="flex mt-3 ">
+                <div className="w-1/2 mr-3 bg-white dark:bg-slate-900">
+                    <div className="m-5 text-[#487ec1] font-bold">近八天系统访问记录</div>
+                    <div className="ml-5" ref={ref} />
+                </div>
+                <div className="w-1/2 pl-3 bg-white dark:bg-slate-900">
+                    <Tabs defaultActiveKey="1" items={items} />
                 </div>
             </section>
         </div>
