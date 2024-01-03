67c5e19a7e277c144a35f6a38631bd7437412b5b
kangod <kangod.yan@gmail.com>
2024-01-03 20:10:05 +0800

    modify: 首页 PV 统计图表去掉数字显示

--- src/views/home/list.page.tsx
+++ src/views/home/list.page.tsx
@@ -112,17 +112,6 @@ export default () => {
             // color: ['#1ca9e6', '#f88c24'],
             /** 设置间距 */
             // marginRatio: 0.1,
-            label: {
-                // 可配置附加的布局方法
-                layout: [
-                    // 柱形图数据标签位置自动调整
-                    { type: 'interval-adjust-position' },
-                    // 数据标签防遮挡
-                    { type: 'interval-hide-overlap' },
-                    // 数据标签文颜色自动调整
-                    { type: 'adjust-color' },
-                ],
-            },
             // 柱子样式
             // columnStyle: {
             //     radius: [20, 20, 0, 0],
